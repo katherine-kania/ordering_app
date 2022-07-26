a11216cb2ecf1953744ce4a3ff9279f29185d602
katherine-kania <katherine.kania@gmail.com>
2022-07-26 16:06:54 -0400

    item adds in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,14 +11,25 @@ const Cart = (props) => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
     const hasItems = cartCtx.items.length > 0
 
-    const cartItemRemoveHandler = id => {}
+    const cartItemRemoveHandler = item => {
+        cartCtx.addItem({...item, amount: -1})
+    }
 
-    const cartItemAddHandler = item => {}
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({...item, amount: 1})
+    }
 
     const cartItems = (
         <ul className={classes['cart-items']}>
             {cartCtx.items.map((item) =>
-                <CartItem key={item.id} name={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item)} onAdd={cartItemAddHandler.bind(null, item)}/>
+                <CartItem 
+                    key={item.id} 
+                    name={item.name} 
+                    amount={item.amount}
+                    price={item.price} 
+                    onRemove={cartItemRemoveHandler.bind(null, item)} 
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
             )}
         </ul>
     );
